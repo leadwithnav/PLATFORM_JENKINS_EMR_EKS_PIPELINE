0c849c838f755ef2d293bbc868e31e9d27a97e8b
unknown <navdeep220161@gmail.com>
2026-07-15 16:14:49 +0530

    changes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -32,24 +32,20 @@ provider "aws" {
 }
 
 # Fetch credentials dynamically from EKS module for K8s/Helm providers
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
